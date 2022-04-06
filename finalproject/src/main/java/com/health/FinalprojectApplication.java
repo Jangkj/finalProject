6c37f0c65ba29f6d4d998d864aa4f84ddc736e13
JangKJ <erioll0@naver.com>
2022-04-06 15:28:46 +0900

    진짜 초안 ㅠ

---
+++ finalproject/src/main/java/com/health/FinalprojectApplication.java
@@ -0,0 +1,18 @@
+package com.health;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.health.dao.MemberDAO;
+
+@SpringBootApplication
+@MapperScan(basePackageClasses = MemberDAO.class)
+public class FinalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalprojectApplication.class, args);
+	}
+
+}
