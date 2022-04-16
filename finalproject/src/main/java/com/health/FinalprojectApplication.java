d65db41680948e3065a8a4e7cc0c8f31f4d68351
JangKJ <erioll0@naver.com>
2022-04-17 01:11:32 +0900

    asdf

--- finalproject/src/main/java/com/health/FinalprojectApplication.java
+++ finalproject/src/main/java/com/health/FinalprojectApplication.java
@@ -1,16 +1,21 @@
 package com.health;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+<<<<<<< HEAD
 import org.springframework.context.annotation.ComponentScan;
 
 import com.health.controller.MemberController;
 import com.health.controller.ProductController;
 import com.health.dao.MemberDAO;
+=======
+>>>>>>> refs/heads/erioll0
 
 @SpringBootApplication
+<<<<<<< HEAD
 @MapperScan(basePackageClasses = MemberDAO.class)
+=======
+>>>>>>> refs/heads/erioll0
 public class FinalprojectApplication {
 
 	public static void main(String[] args) {
