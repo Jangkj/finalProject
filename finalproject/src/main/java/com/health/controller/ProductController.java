7383aaf261821f7bacb7db3084aa850dd09e889e
me <junwoo0909@naver.com>
2022-04-27 01:21:34 +0900

    test1

---
+++ finalproject/src/main/java/com/health/controller/ProductController.java
@@ -0,0 +1,59 @@
+package src.main.java.com.health.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.health.dto.CategoryDTO;
+import com.health.dto.ProductDTO;
+import com.health.service.CategoryService;
+import com.health.service.ProductService;
+
+@Controller
+public class ProductController {
+	@Autowired
+	ProductService service;
+	
+	@Autowired
+	@Qualifier("categoryservice")
+	CategoryService cservice;
+	
+	@RequestMapping(value="/productlist")
+	public ModelAndView productlist(Model model,
+			@RequestParam(defaultValue = "1") String pagenum,
+			@RequestParam(defaultValue = "9") String contentnum,
+            @RequestParam(defaultValue = "category_num") String categorynum) throws Exception{
+		ModelAndView mv= new ModelAndView();
+		System.out.println("------------------");
+		System.out.println(categorynum);
+		System.out.println("------------------");
+		service.execute(model, pagenum, contentnum, categorynum);
+		List<CategoryDTO> clist = cservice.categorylist();
+		mv.addObject("categorylist", clist);
+		mv.setViewName("/productlist");
+		return mv;
+	}
+	
+	//?ÉÅ?Ñ∏?†ïÎ≥? ?éò?ù¥Ïß?
+	@GetMapping(value="productdetail")
+	public ModelAndView productdetail(@RequestParam(value="prod_num") int prod_num ) {
+		ModelAndView mv = new ModelAndView();
+		ProductDTO ProductDTO;
+		try {
+			ProductDTO =service.getproduct(prod_num);
+			mv.addObject("product" , ProductDTO );
+			mv.setViewName("productdetail");
+		} catch (Exception e) {
+		}
+		return mv;
+	}
+	
+}
+
